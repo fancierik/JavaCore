2913d71532772f1ca161c7fdd5ffd3141ff6d46c
Ann Ovcharuk <anblackdiamond@gmail.com>
2022-09-25 18:09:20 +0300

    init

--- JCCul/src/Main.java
+++ JCCul/src/Main.java
@@ -1,8 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        OnTaskDoneListener listener = System.out::println;
-        OnTaskErrorListener listener2 = System.out::println;
-        Worker worker = new Worker(listener, listener2);
-        worker.start();
+        Calculator calc = Calculator.instance.get();
+
+        int a = calc.plus.apply(1, 2);
+        int b = calc.minus.apply(1, 1);
+        int c = calc.devide.apply(a, b);
+
+        calc.println.accept(c);
     }
 }
+// отсутствует проверка деления на ноль,
+// можно реализовать ее тернарным оператором,
+// либо прописать лямбда-выражение с выводом сообщения о недопустимости
+// деления на ноль
